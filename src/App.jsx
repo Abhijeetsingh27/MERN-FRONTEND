c874b97cdd5578da0259c9ccbfe5c12d022a7c37
Abhijeetsingh27 <abhijeetsingh271103@gmail.com>
2025-07-15 09:25:38 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Register from "./components/Register";
+import Product from "./components/Product";
+import Cart from "./components/Cart";
+import Order from "./components/Order";
+import Admin from "./components/Admin";
+import Users from "./components/Users";
+import Orders from "./components/Orders";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import Products from "./components/Products";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
-import Register from "./Register.jsx";
-
-
-
-
 function App() {
   return (
     <div className="App-Container">
-      <h1 style={{ backgroundColor: "orange" }}>MERN Frontend</h1>
-      
-      <Register/>
-      <h3>This is footer</h3>
+      <BrowserRouter>
+       <Header/>
+        <Routes>
+          <Route index element={<Product />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="order" element={<Order />} />
+          <Route path="admin" element={<Admin />}>
+            <Route index element={<Users />} />
+            <Route path="products" element={<Products />} />
+            <Route path="orders" element={<Orders />} />
+          </Route>
+        </Routes>
+       <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
